6e6c98143ab92d7dacf0987c56fcf705f0c48336
Kevin Wahome <wahomekevin@gmail.com>
2018-03-23 12:53:46 +0300

    Added migrations for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323093653) do
+ActiveRecord::Schema.define(version: 20180323094709) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "full_names"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  # name validation tests
+
+  # full name validation tests
+
+  # 
 end
